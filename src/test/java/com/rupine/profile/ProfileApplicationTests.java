4b5001872e728e367942ea53eef9bbfe33050b1a
manojsigh <manojsingh.manoj@gmail.com>
2021-02-07 17:16:16 +0530

    First Commit

---
+++ src/test/java/com/rupine/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rupine.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
